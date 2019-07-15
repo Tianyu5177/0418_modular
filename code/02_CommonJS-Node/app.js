8b44e531b26203a18548826da6e5907d900d7f08
tianyu <0318@qq.com>
2019-07-15 17:02:45 +0800

    update

--- code/02_CommonJS-Node/app.js
+++ code/02_CommonJS-Node/app.js
@@ -1,9 +1,13 @@
 let module1 = require('./modules/module1')
 let module2 = require('./modules/module2')
 let module3 = require('./modules/module3')
+let uniq = require('uniq')
+
 
 
 console.log(module1.name)
 console.log(module1.age)
 module2()
 console.log(module3)
+let arr = [4,4,4,1,2,6,5,5,5,5,4,3,11,10,13]
+console.log(uniq(arr))
